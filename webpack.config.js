1daec29bf5e65a0e5d0b6eb4019d6848a264181d
Norma Mendonca <norma.mendonca@zennioptical.com>
2019-02-17 22:49:15 +0530

    [Final: Example 1]

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {},
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
