84e475e031555465b5d9a632f27a22306ea189b2
Norma Mendonca <norma.mendonca@zennioptical.com>
2019-02-21 15:50:11 +0530

    [Adding Two Way Binding]

--- src/App.js
+++ src/App.js
@@ -11,12 +11,22 @@ class App extends Component {
         ]
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (event, newName) => {
         this.setState({
             persons: [
-                {name: 'Conrad Prem Francis Dsouza', age: 40},
-                {name: 'Norma Francia Mendonca', age: 36},
-                {name: 'Cheryl Nicole Dsouza', age: 10},
+                {name: 'Conrad Dsouza', age: 40},
+                {name: newName, age: 36},
+                {name: 'Cheryl Dsouza', age: 10},
+            ]
+        });
+    }
+
+    nameChangedHandler = (event, index) => {
+        this.setState({
+            persons: [
+                {name: (index===0) ? event.target.value:'Conrad Dsouza', age: 40},
+                {name: (index===1) ? event.target.value:'Norma Mendonca', age: 36},
+                {name: (index===2) ? event.target.value:'Cheryl Dsouza', age: 10},
             ]
         });
     }
@@ -26,10 +36,20 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <button onClick={(event)=> this.switchNameHandler(event, 'Hello Man!')}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                    changed={(event) => this.nameChangedHandler(event, 0)} />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, event, 'Hey Man!')}
+                    changed={(event) => this.nameChangedHandler(event, 1)}>My Hobbies: Racing</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                    changed={(event) => this.nameChangedHandler(event, 2)}/>
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
