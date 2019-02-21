bc670b11f91b7da2c5b095fe8b5378848846529a
Norma Mendonca <norma.mendonca@zennioptical.com>
2019-02-21 17:08:13 +0530

    [Styling with Stylesheets]

--- src/App.js
+++ src/App.js
@@ -32,11 +32,20 @@ class App extends Component {
     }
 
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px'
+        };
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
-                <button onClick={(event)=> this.switchNameHandler(event, 'Hello Man!')}>Switch Name</button>
+                <button
+                    style={style}
+                    onClick={(event)=> this.switchNameHandler(event, 'Hello Man!')}>Switch Name</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age}
