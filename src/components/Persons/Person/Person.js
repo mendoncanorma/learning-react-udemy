18d2f063f1e239f7c774dc1bdfbabff0825714aa
Norma Mendonca <norma.mendonca@zennioptical.com>
2019-03-02 18:42:31 +0530

    [React.fragment]

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import classes from './Person.css';
 
 import Aux from '../../../hoc/Auxiliary';
@@ -13,11 +13,11 @@ const person = ( props ) => {
     }
 
     return (
-        <Aux className={classes.Person}>
+        <Fragment className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </Aux>
+        </Fragment>
     )
 };
 
